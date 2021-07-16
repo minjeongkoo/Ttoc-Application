d78f5b8baf43f1372b804bead188b5437daaaba2
minjeongKoo <koominjeong99@gmail.com>
2021-07-16 09:55:43 +0900

    Initial commit

---
+++ src/main/java/kr/co/ttoc/application/PxytstController.java
@@ -0,0 +1,12 @@
+package kr.co.ttoc.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PxytstController {
+    @GetMapping("/api/pxytst")
+    public String test() {
+        return  "ttoc ttoc";
+    }
+}
