b4bd266d5006a9cb2a21f83a61287056e93c2144
minjeongKoo <koominjeong99@gmail.com>
2021-07-20 15:26:39 +0900

    header 컴포넌트 생성
    App.vue에서 header 컴포넌트 설정
    공통 스타일시트 생성, 연결

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,3 +1,17 @@
 <template>
+  <app-header/>
   <router-view />
 </template>
+
+<script>
+import Header from "./components/Header";
+
+export default {
+  components: {
+    'app-header': Header
+  }
+}
+</script>
+
+<!--Common CSS-->
+<style src="../src/style/ttoc-style.css" type="text/css"/>
---
+++ src/front/src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="q-pa-md q-gutter-y-sm">
+    <q-toolbar>
+      <q-btn flat round dense icon="menu" v-on:click="opneQuickMenu()"/>
+      <q-toolbar-title>
+        홈조리원 똑똑
+      </q-toolbar-title>
+        <q-btn outline style="color: #834BF9;" label="예약하기" />
+    </q-toolbar>
+  </div>
+  <quick-menu v-show="quickMenuEvent.open"/>
+</template>
+
+<script>
+import QuickMenu from '../components/QuickMenu'
+
+export default {
+  data: function() {
+    return {
+      quickMenuEvent: {
+        open: false
+      }
+    }
+  },
+  components: {
+    'quick-menu': QuickMenu
+  },
+  methods: {
+    opneQuickMenu(openEvent) {
+      if (this.quickMenuEvent.open === false) {
+        this.quickMenuEvent.open = true;
+      }else {
+        this.quickMenuEvent.open = false;
+      }
+    }
+  }
+
+}
+
+</script>
