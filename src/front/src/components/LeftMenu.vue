24f5d3a0a19ba1a5d4f951784cf09b1c2d82f3ba
minjeongKoo <koominjeong99@gmail.com>
2021-07-21 18:56:05 +0900

    left menu vue에 똑똑 서비스 아이콘 리스트 영역 컴포넌트로 분리

--- src/front/src/components/LeftMenu.vue
+++ src/front/src/components/LeftMenu.vue
@@ -21,58 +21,7 @@
           </span>
         </div>
 
-        <div class="left-ttoc-service-list">
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon color="blue-grey-10" name="playlist_add_check_circle" />
-            </q-item-section>
-
-            <q-item-section>딱 맞춤 서비스</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon color="blue-grey-10" name="pregnant_woman" />
-            </q-item-section>
-
-            <q-item-section>산전 • 산후 바디관리</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon color="blue-grey-10" name="foundation" />
-            </q-item-section>
-
-            <q-item-section>홈 필라테스</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon color="blue-grey-10" name="nights_stay" />
-            </q-item-section>
-
-            <q-item-section>싱잉볼 • 명상</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon color="blue-grey-10" name="local_dining" />
-            </q-item-section>
-
-            <q-item-section>건강식 배송</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon color="blue-grey-10" name="living" />
-            </q-item-section>
-
-            <q-item-section>홈 클리닉</q-item-section>
-          </q-item>
-
-        </div>
-
+        <service-icon-list/>
 
         <q-separator />
 
@@ -146,6 +95,14 @@
 </template>
 
 <script>
+import ServiceIconList from '../components/ServiceIconList'
+
+
+export default {
+  components: {
+    'service-icon-list': ServiceIconList
+  }
+}
 
 </script>
 
