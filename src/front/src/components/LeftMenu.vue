c7f15fcc1a25ddebcd27323373a2d6bf731f1b72
minjeongKoo <koominjeong99@gmail.com>
2021-07-23 18:58:41 +0900

    componant 분리해둔 내용 다시 통합함.

--- src/front/src/components/LeftMenu.vue
+++ src/front/src/components/LeftMenu.vue
@@ -21,7 +21,57 @@
           </span>
         </div>
 
-        <service-icon-list/>
+        <div class="left-ttoc-service-list">
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="blue-grey-10" name="playlist_add_check_circle" />
+            </q-item-section>
+
+            <q-item-section>딱 맞춤 서비스</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="blue-grey-10" name="pregnant_woman" />
+            </q-item-section>
+
+            <q-item-section>산전 • 산후 바디관리</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="blue-grey-10" name="foundation" />
+            </q-item-section>
+
+            <q-item-section>홈 필라테스</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="blue-grey-10" name="nights_stay" />
+            </q-item-section>
+
+            <q-item-section>싱잉볼 • 명상</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="blue-grey-10" name="local_dining" />
+            </q-item-section>
+
+            <q-item-section>건강식 배송</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="blue-grey-10" name="living" />
+            </q-item-section>
+
+            <q-item-section>홈 클리닉</q-item-section>
+          </q-item>
+
+        </div>
 
         <q-separator />
 
@@ -94,14 +144,6 @@
 </template>
 
 <script>
-import ServiceIconList from '../components/ServiceIconList'
-
-
-export default {
-  components: {
-    'service-icon-list': ServiceIconList
-  }
-}
 
 </script>
 
